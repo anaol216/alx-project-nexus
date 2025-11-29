25a5721ecd7a961a50b342d8928a8ddf4f5d9b47
anaol216 <atinafuanaol@gmail.com>
2025-11-29 10:51:37 +0300

    feat: add API integration for fetching product data

--- dynamic-c-commerce-product/store/slices/productSlice.ts
+++ dynamic-c-commerce-product/store/slices/productSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { Product } from '../../types';
+import { Product } from '../../interfaces';
 
 interface ProductState {
     items: Product[];
